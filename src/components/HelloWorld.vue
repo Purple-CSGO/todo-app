8edb690f6fcb1cb0bbe65e0d93725edf901eb343
Purp1e <47248616+Purple-CSGO@users.noreply.github.com>
2023-07-17 22:00:32 +0800

    turn count++ into a function call

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,18 @@ defineProps({
 })
 
 const count = ref(0)
+
+const count_plus = () => {
+    console.log(count)
+    count.value++
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count_plus()">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
